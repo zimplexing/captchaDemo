2e82d905c20b2a4b325772185c7ced0f7180bd59
zimplex <zhangx0728@gmail.com>
2016-07-01 14:10:21 +0800

    upload project

---
+++ public/javascripts/main.js
@@ -0,0 +1,32 @@
+jQuery(document).ready(function($) {
+	$('#login').click(function(event) {
+		/* Act on the event */
+		$.ajax({
+				url: '/verfiy',
+				type: 'GET',
+				data: {
+					"userName": $('.userName').val(),
+					"password": $('.password').val(),
+					"captcha": $('.captcha').val()
+				},
+			})
+			.done(function(data) {
+				if (data) {
+					alert('captcha right!');
+				} else {
+					alert('captcha wrong!');
+				}
+			})
+	});
+	$('#captcha').on('click','img',function(event) {
+		$('#captcha img').remove();
+		$('#captcha').append('<img src="/getCaptcha?_'+ (new Date()).getTime() +'" />')
+		// $.ajax({
+		// 	url:'/getCaptcha',
+		// 	type:'post',
+		// 	success:function(data){
+		// 		console.log(data);
+		// 	}
+		// })
+	});
+});
